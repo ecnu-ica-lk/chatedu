1bf9597ad43664bb89f2a1df6884282bcbe38bfa
apm29 <yjw999wow@163.com>
2023-04-14 09:57:54 +0800

    dep: add auto-resolve for ele-plus

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
+// import ElementPlus from "element-plus";
+// import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-import zhCn from "element-plus/lib/locale/lang/zh-cn"; //国际化
+// import zhCn from "element-plus/lib/locale/lang/zh-cn"; //国际化
 import { createPinia } from "pinia";
 const app = createApp(App);
 //实例化pinia
 const pinia = createPinia();
 
-app.use(ElementPlus, { locale: zhCn }).use(router).use(pinia).mount("#app");
+app.use(router).use(pinia).mount("#app");
 //全局注册图标组件
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
