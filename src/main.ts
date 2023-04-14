1683344235f795c2a0e34cfa7756e00da07d4b3e
apm29 <yjw999wow@163.com>
2023-04-14 10:09:35 +0800

    chore(dep): remove useless icon import

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 // import ElementPlus from "element-plus";
 // import "element-plus/dist/index.css";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+// import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 // import zhCn from "element-plus/lib/locale/lang/zh-cn"; //国际化
 import { createPinia } from "pinia";
 const app = createApp(App);
@@ -12,7 +12,7 @@ const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router).use(pinia).mount("#app");
-//全局注册图标组件
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
-}
+// //全局注册图标组件
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//   app.component(key, component);
+// }
